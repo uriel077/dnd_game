26c3bb1cb6456b337e18f20b7d1817973cfdf9a3
uriel07777 <uriel07777@gmail.com>
2021-05-30 19:35:29 +0300

    create UI add more functions

--- src/handlers/InputHandler.java
+++ src/handlers/InputHandler.java
@@ -1,32 +1,34 @@
 package handlers;
 
 import enums.UserInput;
+import game.DatabaseUnits;
 
 import java.util.Scanner;
 
 public class InputHandler {
 
     private static String userInputRegex=UserInput.CastAbility.getRegex();
-    private static String menuRegex="[1-7]";
-    public static char inputPlayerGame(){
-        return inputCach(userInputRegex);
+    private static String menuRegex="[1-"+ DatabaseUnits.playerPool.size() +"]";
+
+    public static UserInput inputPlayerGame(){
+        return UserInput.valueOf(inputCache(userInputRegex)+"");
 
     }
     public static  char inputMenu(){
-    return inputCach(menuRegex);
-
+    return inputCache(menuRegex);
     }
-    private static char inputCach(String regex){
+
+    private static char inputCache(String regex){
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         String input="";
         do{
             input=myObj.nextLine();
-        }while (!validateCharWithRegex(input,regex));
+        }while (!validateWithRegex(input,regex));
         return input.charAt(0);
     }
 
 
-    private static Boolean validateCharWithRegex(String s,String regex){
+    private static Boolean validateWithRegex(String s,String regex){
         if(s.length()==1)
             if(s.matches(regex))
                 return true;
--- src/main.java
+++ src/main.java
@@ -1,17 +1,8 @@
-import game.Coordinate;
-import game.DatabaseUnits;
 import game.GameManager;
-import game.Health;
-import handlers.TargetHandler;
-import unit.player.Rogue;
 
 public class main {
     public static void main(String[] args) {
-        DatabaseUnits db=new DatabaseUnits();
-        Health h =new Health(1,5);
-        Rogue rog=new Rogue("",'w',1,5,5,5);
-        TargetHandler.candidateTarget(rog,new Coordinate(),8);
-        String address="D:\\study\\data_structre\\projects\\levels_dir";
+        String address="D:\\projects\\levels_dir";
         GameManager gameManager=new GameManager();
         gameManager.start(address);
 
