d991d85ffa1e5a882b719450ecfa4033db2b6a38
Erlich <d7erlichman@gmail.com>
2021-06-01 01:40:02 +0300

    fixed some problems.

--- src/handlers/MoveHandler.java
+++ src/handlers/MoveHandler.java
@@ -8,19 +8,22 @@ import unit.Unit;
 import unit.enemy.Enemy;
 import unit.player.Player;
 
+import java.util.List;
+
 public class MoveHandler {
     public static GameBoard gameBoard;
 
     public void move(UserInput input, Player player){
         Coordinate moveTo = joyStick(input, player.getCoordinate());
-        Enemy target = isAttack(moveTo, player);
+        Enemy target = targetCondidate(moveTo, player);
         if (target == null){
             player.setCoordinate(moveTo);
         }
         else {
-            UI.print(player.getName() + " engaged in combat with " + target.getName());
-            UI.print(player.description());
-            UI.print(target.description());
+//            UI.print(player.getName() + " engaged in combat with " + target.getName());
+//            UI.print(player.description());
+//            UI.print(target.description());
+            combat(player, target);
             player.attack(target);
         }
 
@@ -28,16 +31,24 @@ public class MoveHandler {
 
     public void move(UserInput input, Enemy enemy){
         Coordinate moveTo = joyStick(input, enemy.getCoordinate());
-        Player target = isAttack(moveTo, enemy);
+        Player target = targetCondidate(moveTo, enemy);
         if (target == null){
             enemy.setCoordinate(moveTo);
         }
         else {
-            UI.print(enemy.getName() + " engaged in combat with " + target.getName());
-            UI.print(enemy.description());
-            UI.print(target.description());
+//            UI.print(enemy.getName() + " engaged in combat with " + target.getName());
+//            UI.print(enemy.description());
+//            UI.print(target.description());
+            combat(enemy, target);
             enemy.attack(target);
         }
+
+    }
+
+    public void combat(Unit unit1, Unit unit2){
+        UI.print(unit1.getName() + " engaged in combat with " + unit2.getName());
+        UI.print(unit1.description());
+        UI.print(unit2.description());
 
     }
 
@@ -58,11 +69,13 @@ public class MoveHandler {
                 !gameBoard.walls.containsKey(newPos) );
     }
 
-    private Enemy isAttack(Coordinate newPos, Player player){
-        return TargetHandler.candidateTarget(player, 0).get(0);
+    private Enemy targetCondidate(Coordinate newPos, Player player){
+        List<Enemy> targets = TargetHandler.candidateTarget(player, 0);
+        return targets.size() == 0 ? null: targets.get(0);
     }
 
-    private Player isAttack(Coordinate newPos, Enemy player){
-        return TargetHandler.candidateTarget(player, 0).get(0);
+    private Player targetCondidate(Coordinate newPos, Enemy enemy){
+        List<Player> targets = TargetHandler.candidateTarget(enemy, 0);
+        return targets.size() == 0 ? null: targets.get(0);
     }
 }
