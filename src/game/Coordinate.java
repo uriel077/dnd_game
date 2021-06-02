a0b402318fea2c985ddf493e903a86026b34d68c
Erlich <d7erlichman@gmail.com>
2021-06-02 15:21:26 +0300

    fixed wall bug.

--- src/game/Coordinate.java
+++ src/game/Coordinate.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.Objects;
+
 public class Coordinate {
     public int x=0;
     public int y=0;
@@ -21,5 +23,18 @@ public class Coordinate {
     @Override
     public String toString() {
         return "x:"+x+", y:"+y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
