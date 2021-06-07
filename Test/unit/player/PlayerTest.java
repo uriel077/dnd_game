09e1a1ff4d11c431b838e8276a434f5c388d6b5c
Erlich <d7erlichman@gmail.com>
2021-06-07 13:28:34 +0300

    testing!

---
+++ Test/unit/player/PlayerTest.java
@@ -0,0 +1,68 @@
+package unit.player;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void description() {
+    }
+
+    @Test
+    public void getExperience() {
+    }
+
+    @Test
+    public void levelUp() {
+    }
+
+    @Test
+    public void setExperience() {
+    }
+
+    @Test
+    public void castAbility() {
+    }
+
+    @Test
+    public void attack() {
+    }
+
+    @Test
+    public void setAbilityName() {
+    }
+
+    @Test
+    public void tryCastAbility() {
+    }
+
+    @Test
+    public void testTryCastAbility() {
+    }
+
+    @Test
+    public void move() {
+    }
+
+    @Test
+    public void turn() {
+    }
+
+    @Test
+    public void testCastAbility() {
+    }
+
+    @Test
+    public void copy() {
+    }
+
+    @Test
+    public void setHealth() {
+    }
+}
