74e99a8e383e9ca6ab614d511131893529385da2
uriel077 <uriel07777@gmail.com>
2021-06-09 17:12:06 +0300

    fix move attack bug

---
+++ Test/game/CoordinateTest.java
@@ -0,0 +1,37 @@
+package game;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import unit.Unit;
+import unit.enemy.Enemy;
+import unit.player.Player;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoordinateTest {
+
+    Coordinate cord1;
+    Coordinate cord2;
+    Coordinate cord3;
+    Coordinate cord4;
+    @BeforeEach
+    public void setUp() throws Exception {
+        cord1=new Coordinate(1,2);
+        cord2=new Coordinate(2,2);
+        cord3=new Coordinate(1,2);
+        cord4=new Coordinate(4,6);
+
+    }
+
+
+    @Test
+    void distance() {
+        assertEquals(cord1.distance(cord3),0);
+        assertEquals(cord3.distance(cord1),0);
+        assertEquals(cord1.distance(cord1),0);
+        assertEquals(cord3.distance(cord4),5);
+
+    }
+}
--- Test/unit/player/PlayerTest.java
+++ Test/unit/player/PlayerTest.java
@@ -1,68 +1,80 @@
 package unit.player;
 
+import game.DatabaseUnits;
+import game.GameManager;
+import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import unit.Unit;
+import unit.enemy.Enemy;
 
 public class PlayerTest {
 
-    @Before
-    public void setUp() throws Exception {
+    public  Player rogue;
+    public  Player mage;
+    public  Player hunter;
+    public  Player warrior;
+    public Enemy trap;
+    public Enemy monster;
+    public Enemy superDuperMonster;
 
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        Unit.gameManager=new GameManager();
+        DatabaseUnits.buildDictionary();
+        warrior=(Player) DatabaseUnits.playerPool.get("1").copy();
+        mage=(Player) DatabaseUnits.playerPool.get("3").copy();
+        rogue=(Player) DatabaseUnits.playerPool.get("5").copy();
+        hunter=(Player) DatabaseUnits.playerPool.get("7").copy();
+        trap=(Enemy) DatabaseUnits.enemyPool.get("B").copy();
+        monster=(Enemy) DatabaseUnits.enemyPool.get("k").copy();
+        superDuperMonster=new Enemy("Super",'D',2000000,0,200000,20);
     }
 
     @Test
     public void description() {
+        Assert.assertEquals(warrior.description(),"Jon Snow\t\tHealth: 300/300\t\tAttack: 30\t\tDefence: 4\t\tLevel: 1\t\tExperience:0/50\t\tCooldown: 0/3");
+        Assert.assertEquals(mage.description(),"Melisandre\t\tHealth: 100/100\t\tAttack: 5\t\tDefence: 1\t\tLevel: 1\t\tExperience:0/50\t\tMana: 75/300\t\tspellPower: 15");
+        Assert.assertEquals(rogue.description(),"Arya Stark\t\tHealth: 150/150\t\tAttack: 40\t\tDefence: 2\t\tLevel: 1\t\tExperience:0/50\t\tEnergy: 100/100");
+        Assert.assertEquals(hunter.description(),"Ygritte\t\tHealth: 220/220\t\tAttack: 30\t\tDefence: 2\t\tLevel: 1\t\tExperience:0/50\t\tArrows: 10");
     }
 
     @Test
     public void getExperience() {
+        Assert.assertEquals(warrior.getExperience(),0);
+        warrior.setExperience(25);
+        Assert.assertEquals(warrior.getExperience(),25);
+        warrior.setExperience(51);
+        Assert.assertEquals(warrior.getExperience(),1);
+
     }
 
     @Test
     public void levelUp() {
+        Assert.assertEquals(mage.playerLevel,1);
+        mage.setExperience(50);
+        Assert.assertEquals(mage.playerLevel,2);
+        mage.setExperience(250);
+        Assert.assertEquals(mage.playerLevel,4);
+
     }
 
-    @Test
-    public void setExperience() {
-    }
-
-    @Test
-    public void castAbility() {
-    }
 
     @Test
     public void attack() {
-    }
+        Assert.assertEquals(rogue.playerLevel, 1);
+        while (!monster.isDead()) {
+            rogue.attack(monster);
+        }
+        Assert.assertEquals(rogue.playerLevel, 2);
 
-    @Test
-    public void setAbilityName() {
     }
-
-    @Test
-    public void tryCastAbility() {
-    }
-
-    @Test
-    public void testTryCastAbility() {
-    }
-
-    @Test
-    public void move() {
-    }
-
-    @Test
-    public void turn() {
-    }
-
-    @Test
-    public void testCastAbility() {
-    }
-
-    @Test
-    public void copy() {
-    }
-
     @Test
     public void setHealth() {
+        Assert.assertEquals(hunter.getCurrentHealth(),220);
+        hunter.setHealth(hunter.getCurrentHealth()-5, hunter.getCurrentHealth());
+        Assert.assertEquals(hunter.getCurrentHealth(),215);
     }
 }
